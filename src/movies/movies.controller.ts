70d022763f96039a53076371be32c71314cc4e08
Shin hyun <gusals0702@naver.com>
2022-06-28 15:15:37 +0900

    hi-nest 최종

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,36 +1,45 @@
-import { Controller, Delete, Get, Param, Post, Patch, Body, Query } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
-import { Movie } from './entities/movie.entity';
-import { MoviesService } from './movies.service';
-
-@Controller('movies')
-export class MoviesController {
-
-    constructor(readonly movieService: MoviesService) {}
-
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    Patch,
+    Body,
+    Req,
+    Res,
+  } from '@nestjs/common';
+  import { MoviesService } from './movies.service';
+  import { Movie } from './entities/movie.entity';
+  import { CreateMovieDto } from './dto/create-movie.dto';
+  import { UpdateMovieDto } from './dto/update-movie.dto';
+  
+  @Controller('movies')
+  export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+  
     @Get()
-    getAll():Movie[]{
-        return this.movieService.getAll();
-    };
-
+    getAll(): Movie[] {
+      return this.moviesService.getAll();
+    }
+  
     @Get(':id')
-    getOne(@Param('id') movieId: number): Movie{
-        return this.movieService.getOne(movieId);
-    };
-
+    getOne(@Param('id') movieId: number): Movie {
+      return this.moviesService.getOne(movieId);
+    }
+  
     @Post()
-    create(@Body() movieData: CreateMovieDto){
-        return this.movieService.create(movieData);
+    create(@Body() movieData: CreateMovieDto) {
+      return this.moviesService.create(movieData);
     }
-
+  
     @Delete(':id')
-    deleteMovie(@Param('id') movieId: number){
-        return this.movieService.deleteOne(movieId);
+    remove(@Param('id') movieId: number) {
+      return this.moviesService.deleteOne(movieId);
     }
-
+  
     @Patch(':id')
-    path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto){
-        return this.movieService.update(movieId, updateData);
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+      return this.moviesService.update(movieId, updateData);
     }
-}
+  }
